b6c134043273fa6b0e567d2d42e80307c2a9d73d
AkhtarHussain-nuclei <akhtar.hussain@gonuclei.com>
2020-05-09 16:32:10 +0530

    latest

---
+++ src/test/java/testVagrant/Utils.java
@@ -0,0 +1,22 @@
+package testVagrant;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utils {
+    public Properties fetchFromPropertiesFile(){
+        Properties properties = new Properties();
+        try {
+            File file = new File("src/test/resources/config.properties");
+            FileInputStream fileInput = new FileInputStream(file);
+            properties.load(fileInput);
+            fileInput.close();
+        }catch(IOException ioe){
+            ioe.printStackTrace();
+        }
+
+        return properties;
+    }
+}
