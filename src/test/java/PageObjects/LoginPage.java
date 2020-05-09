64211160f1554b98d9a077cefac3d1f973c90b6b
AkhtarHussain-nuclei <akhtar.hussain@gonuclei.com>
2020-05-09 16:55:40 +0530

    latest

---
+++ src/test/java/PageObjects/LoginPage.java
@@ -0,0 +1,35 @@
+package PageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.CacheLookup;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    public WebDriver localDriver;
+
+    public LoginPage(WebDriver rdriver){
+        localDriver = rdriver;
+        PageFactory.initElements(rdriver, this);
+    }
+
+  @FindBy(css = ".password-login a")
+    @CacheLookup
+    WebElement passwordfield;
+
+  @FindBy(id = "Password")
+    @CacheLookup
+    WebElement passwordTextField;
+
+  @FindBy(name = "commit")
+    @CacheLookup
+    WebElement SubmitPassword;
+
+  public void loginWithPassWord(){
+      passwordfield.click();
+      passwordTextField.sendKeys("idgad");
+      SubmitPassword.click();
+  }
+
+}
--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,5 +1,3 @@
-package testVagrant;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
